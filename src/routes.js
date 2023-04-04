ab57e86a1391443492c60f0520a256de8dbee047
PedroHercules <pedroherculesdantas@gmail.com>
2023-04-03 22:09:41 -0300

    feat(routes): added create and get tasks routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,31 @@
+import { Database } from "./database/database.js"
+
+const database = new Database()
+
 export const routes = [
   {
     method: 'GET',
     path: '/',
     handler: (req, res) => {
       res.end('Hello world!')
+    }
+  },
+  {
+    method: 'POST',
+    path: '/task',
+    handler: (req, res) => {
+      const { title, description } = req.body
+      database.create('tasks', { title, description })
+      res.writeHead(201).end('Task created!')
+    }
+  },
+  {
+    method: 'GET',
+    path: '/tasks',
+    handler: (req, res) => {
+      const tasks = database.findAll('tasks')
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(tasks))
     }
   }
 ]
